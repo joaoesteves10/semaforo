868879f90921724b0673b18af9adf847718813ee
sonicalterno <blbealeite@gmail.com>
2023-05-02 10:44:10 +0100

    nomes dos jogadores e regras do jogo

---
+++ main.py
@@ -0,0 +1,12 @@
+# menu ainda não organizado com alguns prints
+
+print("Bem-vindos ao Semáforo!!")
+player1 = input("Insira o nome do jogador 1: ")
+player2 = input("Insira o nome do jogador 2: ")
+
+print("O objetivo deste jogo é ser o primeiro a conseguir uma linha de três peças da mesma cor na horizontal, vertical ou diagonal.")
+print("O jogo realiza-se num tabuleiro, inicialmente vazio. Em cada jogada, cada jogador realiza uma das seguintes ações:")
+print("-> Colocar uma peça verde num quadrado vazio;")
+print("-> Substituir uma peça verde por uma peça amarela;")
+print("-> Substituir uma peça amarela por uma peça vermelha.")
+print("É importante realçar que as peças vermelhas não podem ser substituídas, o que significa que à medida que o tabuleiro fica com peças vermelhas, é inevitável que surja uma linha de três peças.")
