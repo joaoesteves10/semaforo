c590e8881c14bdc9b2e8f267537169483dc62687
João Esteves <joaoesteves10A4@sapo.pt>
2023-05-19 17:58:58 +0100

    passa a lógica do jogo para um ficheiro separado

---
+++ logicaSemaforo.py
@@ -0,0 +1,58 @@
+## módulo com a lógica para o jogo do semáforo
+import random
+
+def initGameData(player1, player2):
+    return {
+        "playerNames": [player1, player2],
+        "turn": random.randint(0,1),
+        "board": [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+    }
+
+def checkAvailablePieces(gameData, play):
+    counts = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0
+    }
+
+    testGameBoard = []
+    for bl in gameData["board"]:
+        testGameBoard.append(bl.copy())
+    testGameBoard[int(play[0])-1][int(play[1])-1] += 1
+    for i in range(3):
+        for j in range(4):
+            counts[testGameBoard[i][j]] += 1
+
+    del counts[0]
+    for i in counts.values():
+        if i > 8:
+            return False
+
+    return True
+
+
+def passarVez(gameData):
+    gameData["turn"] = (gameData["turn"] + 1) % 2
+
+def checkWin(gameData):
+    board = gameData["board"]
+
+    for row in gameData["board"]:
+        if row[0] == row[1] == row[2] != 0:
+            return True
+
+    for col in range(4):
+        if board[0][col] == board[1][col] == board[2][col] != 0:
+            return True
+
+    if board[0][0] == board[1][1] == board[2][2] != 0:
+        return True
+    if board[0][2] == board[1][1] == board[2][0] != 0:
+        return True
+    if board[0][1] == board[1][2] == board[2][3] != 0:
+        return True
+    if board[0][3] == board[1][2] == board[2][1] != 0:
+        return True
+
+    return False
--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import sys, os, random
 from termcolor import colored, cprint
-
-gameData = {
-    "playerNames": [], # lista dos nomes, playerNames[0] para o player 1, playerNames[1] para o player 2
-    "turn": 0,
-    "board": [[0,0,0,0],[0,0,0,0],[0,0,0,0]] # board como lista de listas, cada lista representa uma linha, board[1][2] é a segunda linha, terceira coluna
-}
+import logicaSemaforo as s
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -28,7 +23,7 @@ def mainMenu():
     print("4: Sair da aplicação")
     escolha = int(input("\nEscolha uma opção: "))
     match escolha:
-        case 1: novoJogoPvP(gameData) # implementar PvB e BvB depois
+        case 1: novoJogoPvP() # implementar PvB e BvB depois
         # case 2: carregarJogo() # ainda não implementado
         case 3: regras()
         case 4: sys.exit()
@@ -52,44 +47,16 @@ def printPiece(piece):
         case 2: print(colored("2", "yellow"), end="")
         case 3: print(colored("3", "red"), end="")
 
-def novoJogoPvP(gameData):
-    gameData["playerNames"].append(input("Nome do jogador 1: "))
-    gameData["playerNames"].append(input("Nome do jogador 2: "))
-    gameData["turn"] = random.randint(0,1)
+def novoJogoPvP():
+    player1 = input("Nome do jogador 1: ")
+    player2 = input("Nome do jogador 2: ")
+    gameData = s.initGameData(player1, player2)
     print(f"O jogador", gameData["playerNames"][gameData["turn"]], "joga primeiro.")
+    input("Prima ENTER para começar o jogo.")
     gameLoop(gameData)
 
-
-def checkWin(gameData):
-    return False
-
-def checkAvailablePieces(gameData, play):
-    counts = {
-        0: 0,
-        1: 0,
-        2: 0,
-        3: 0
-    }
-
-    testGameBoard = []
-    for bl in gameData["board"]:
-        testGameBoard.append(bl.copy())
-    testGameBoard[int(play[0])-1][int(play[1])-1] += 1
-    for i in range(3):
-        for j in range(4):
-            counts[testGameBoard[i][j]] += 1
-
-    del counts[0]
-    for i in counts.values():
-        if i > 8:
-            return False
-
-    return True
-
-
-
 def gameLoop(gameData):
-    while not checkWin(gameData):
+    while not s.checkWin(gameData):
         clear()
         printBoard(gameData)
         while True:
@@ -98,7 +65,7 @@ def gameLoop(gameData):
             if len(play) == 2 and play[0] in "123" and play[1] in "1234":
                 if gameData["board"][int(play[0])-1][int(play[1])-1] == 3:
                     print("jogada inválida, não pode substituir uma peça vermelha")
-                elif not checkAvailablePieces(gameData, play):
+                elif not s.checkAvailablePieces(gameData, play):
                     print("jogada inválida, não há peças disponíveis")
                 else:
                     gameData["board"][int(play[0])-1][int(play[1])-1] += 1
@@ -106,5 +73,9 @@ def gameLoop(gameData):
             else:
                 print("jogada inválida")
         gameData["turn"] = (gameData["turn"] + 1) % 2
+    gameData["turn"] = (gameData["turn"] + 1) % 2 # para voltar ao jogador que ganhou
+    print("Fim do jogo")
+    printBoard(gameData)
+    print("O jogador", gameData["playerNames"][gameData["turn"]], "ganhou!")
 
 mainMenu()
